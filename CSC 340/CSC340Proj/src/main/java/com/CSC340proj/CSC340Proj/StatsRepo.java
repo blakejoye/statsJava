8c92a4e60b94139b6ec98c1bdf2073cd10536b74
blakejoye <142560525+blakejoye@users.noreply.github.com>
2023-11-14 16:06:39 -0500

    Add files via upload

---
+++ CSC 340/CSC340Proj/src/main/java/com/CSC340proj/CSC340Proj/StatsRepo.java
@@ -0,0 +1,9 @@
+package com.CSC340proj.CSC340Proj;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StatsRepo extends JpaRepository <Stats,Long> {
+
+}
